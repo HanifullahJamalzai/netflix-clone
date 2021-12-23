a52507348bcf86922553d83c8bb644ebab65f131
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2021-12-23 23:02:21 +0430

    constants directory filled with router names and used in App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import Home from "./pages/Home";
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/browse" element={<h1>I will be the browse page</h1>} />
-			<Route path="/signin" element={<h1>I will be the sign in page</h1>} />
-			<Route path="/signup" element={<h1>I will be the sign up page</h1>} />
+			<Route path={ROUTES.HOME} element={<Home />} />
+			<Route
+				path={ROUTES.BROWSE}
+				element={<h1>I will be the browse page</h1>}
+			/>
+			<Route
+				path={ROUTES.SIGN_IN}
+				element={<h1>I will be the sign in page</h1>}
+			/>
+			<Route
+				path={ROUTES.SIGN_UP}
+				element={<h1>I will be the sign up page</h1>}
+			/>
 		</Routes>
 	);
 }
