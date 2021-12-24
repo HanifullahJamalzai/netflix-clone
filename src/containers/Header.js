ef875862a7d37e9f903932eabcb382b2f6eba98f
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2021-12-24 11:01:28 +0430

    Header component Done ( img import remain the sam as usuall inside style component)

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+
+export function HeaderContainer({ children }) {
+	return (
+		<Header>
+			<Header.Frame>
+				<Header.Logo
+					to={ROUTES.HOME}
+					src={process.env.PUBLIC_URL + "/images/misc/logo.svg"}
+					alt="Netflix"
+				/>
+				<Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+			</Header.Frame>
+			{children}
+		</Header>
+	);
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,22 @@ import React from "react";
 import { FaqsContainer } from "../containers/Faqs";
 import { JumbotronContainer } from "../containers/Jumbotron";
 import { FooterContainer } from "../containers/Footer";
-
+import { HeaderContainer } from "../containers/Header";
+import { OptForm } from "../components";
 export default function Home() {
 	return (
 		<>
+			<HeaderContainer>
+				<OptForm>
+					<OptForm.Input placeholder="Email Address" />
+					<OptForm.Button>Try it now</OptForm.Button>
+					<OptForm.Break />
+					<OptForm.Text>
+						Ready to watch? Enter your email to create or restart your
+						membership.
+					</OptForm.Text>
+				</OptForm>
+			</HeaderContainer>
 			<JumbotronContainer />;
 			<FaqsContainer />
 			<FooterContainer />
