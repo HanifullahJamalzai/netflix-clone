74aa0ea87b6129a865a1565e3854124c1aa7312d
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2021-12-27 19:10:29 +0430

    fuse.js implented for live search

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import Fuse from "fuse.js";
 import { Card, Header, Loading } from "../components";
 import * as ROUTES from "../constants/routes";
 import { FirebaseContext } from "../contexts/firebase";
@@ -28,6 +29,19 @@ export function BrowseContainer({ slides }) {
 	useEffect(() => {
 		setSlideRows(slides[category]);
 	}, [slides, category]);
+
+	useEffect(() => {
+		const fuse = new Fuse(slideRows, {
+			keys: ["data.description", "data.title", "data.genre"],
+		});
+		const results = fuse.search(searchTerm).map(({ item }) => item);
+
+		if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+			setSlideRows(results);
+		} else {
+			setSlideRows(slides[category]);
+		}
+	}, [searchTerm]);
 
 	return profile.displayName ? (
 		<>
